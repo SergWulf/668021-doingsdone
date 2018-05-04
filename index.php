c0fee34895e18ab2362225e9d667edc6cb0d192a
SergWulf <36727388+SergWulf@users.noreply.github.com>
2018-05-04 13:16:16 +0300

    Вывод списка задач с помощью двумерного массива

--- index.php
+++ index.php
@@ -115,49 +115,18 @@ $array_tasks["task_6"]=array("task"=>"Заказать пиццу","date"=>"не
                 </div>
 
                 <table class="tasks">
-                    <tr class="tasks__item task">
+                    <?php foreach ($array_tasks as $task=>$array): ?>
+                    <tr class="tasks__item task  <?php if ($array_tasks[$task]['complete']=="Да"): echo("task--completed"); endif;?>">
                         <td class="task__select">
                             <label class="checkbox task__checkbox">
-                                <input class="checkbox__input visually-hidden task__checkbox" type="checkbox" value="1">
-                                <span class="checkbox__text">Сделать главную страницу Дела в порядке</span>
+                                <input class="checkbox__input visually-hidden task__checkbox" type="checkbox">
+                                <span class="checkbox__text"><?php echo($array_tasks[$task]['task']); ?></span>
                             </label>
                         </td>
-
-                        <td class="task__file">
-                            <a class="download-link" href="#">Home.psd</a>
-                        </td>
-
-                        <td class="task__date"></td>
+                        <td class="task__date"><?php echo($array_tasks[$task]['date']); ?></td>
+                        <td class="task__category"><?php echo($array_tasks[$task]['category']); ?></td>
                     </tr>
-                    <!--показывать следующий тег <tr/>, если переменная $show_complete_tasks равна единице-->
-                    <?php if ($show_complete_tasks==1): ?>
-                    <tr class="tasks__item task task--completed">
-                        <td class="task__select">
-                            <label class="checkbox task__checkbox">
-                                <input class="checkbox__input visually-hidden" type="checkbox" checked>
-                                <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
-                            </label>
-                        </td>
-                        <td class="task__date">10.04.2017</td>
-
-                        <td class="task__controls">
-                        </td>
-                    </tr>
-                    <?php endif; ?>
-                    <tr class="tasks__item task task--completed">
-                        <td class="task__select">
-                            <label class="checkbox task__checkbox">
-                                <input class="checkbox__input visually-hidden task__checkbox" type="checkbox" checked>
-                                <span class="checkbox__text">Сделать главную страницу Дела в порядке</span>
-                            </label>
-                        </td>
-
-                        <td class="task__file">
-                            <a class="download-link" href="#">Home.psd</a>
-                        </td>
-
-                        <td class="task__date"></td>
-                    </tr>
+                    <?php endforeach; ?>
                 </table>
             </main>
         </div>

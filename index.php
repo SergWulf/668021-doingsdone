344c3609c5f1d7f1d3b1a9b19fde3045762839ae
SergWulf <36727388+SergWulf@users.noreply.github.com>
2018-05-14 16:01:59 +0300

    Добавил пробелы между операторами языка

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 require_once('data.php');
 require_once('functions.php');
-$page_content=include_template('templates/index.php', ['array_tasks' => $array_tasks]);
-$layout_content=include_template('templates/layout.php', [
+$page_content = include_template('templates/index.php', ['array_tasks' => $array_tasks]);
+$layout_content = include_template('templates/layout.php', [
     'title' => 'Дела в порядке',
     'name_user' => 'Вячеслав',
     'content' => $page_content,

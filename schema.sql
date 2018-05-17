ad32bd544d71b94cddf4854dbe6c036c14256bcb
SergWulf <36727388+SergWulf@users.noreply.github.com>
2018-05-17 15:47:31 +0300

    Исправил замечания по SQL созданию БД, таблиц и индексов

--- schema.sql
+++ schema.sql
@@ -10,6 +10,15 @@ user_id INT(11),
 name_project CHAR(124)
 );
 
+CREATE TABLE users (
+id INT(11) AUTO_INCREMENT PRIMARY KEY,
+name_user CHAR(64),
+email_user CHAR(254),
+password_user CHAR(254),
+reg_date_user CHAR(64),
+contact_user CHAR(254)
+);
+
 CREATE TABLE tasks (
 id INT(11) AUTO_INCREMENT PRIMARY KEY,
 user_id INT(11),
@@ -18,19 +27,17 @@ name_task CHAR(124),
 create_date_task CHAR(64),
 run_date_task CHAR(64),
 limit_date_task CHAR(64),
-file_task CHAR(254)
+file_task CHAR(254),
+status BOOLEAN,
+FOREIGN KEY (user_id) REFERENCES users(id),
+FOREIGN KEY (project_id) REFERENCES projects(id)
 );
 
-CREATE TABLE users (
-id INT(11) AUTO_INCREMENT PRIMARY KEY,
-name_user CHAR(64),
-email_user CHAR(254),
-password_user CHAR(64),
-reg_date_user CHAR(64),
-contact_user CHAR(254)
-);
 
-CREATE INDEX name_project_index ON projects(name_project);
-CREATE INDEX name_task_index ON tasks(name_task);
-CREATE INDEX name_user_index ON users(name_user);
+
+
+CREATE FULLTEXT INDEX name_task_index ON tasks(name_task);
+CREATE INDEX limit_date_and_status_index ON tasks(limit_date_task,status);
+CREATE INDEX status_index ON tasks(status);
+CREATE INDEX user_and_project_index ON tasks(user_id,project_id);
 CREATE UNIQUE INDEX email_user_index ON users(email_user);

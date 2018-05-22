e0fecaef88e45d3bb5ed1a02c4896291d205588f
SergWulf <36727388+SergWulf@users.noreply.github.com>
2018-05-22 11:12:25 +0300

    Убрал вложенные запросы, добавил дату в нужном формате, исправил другие замечания

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 -- Добавление 3 пользователей
-INSERT INTO users SET name_user = 'Игнат', email_user = 'ignat.v@gmail.com', password_user = '$2y$10$OqvsKHQwr0Wk6FMZDoHo1uHoXd4UdxJG/5UDtUiie00XaxMHrW8ka', reg_date_user = '9.05.2018', contact_user = '+79212343456';
-INSERT INTO users SET name_user = 'Леночка', email_user = 'kitty_93@li.ru', password_user = '$2y$10$bWtSjUhwgggtxrnJ7rxmIe63ABubHQs0AS0hgnOo41IEdMHkYoSVa', reg_date_user = '11.05.2018', contact_user = '+78202254822';
-INSERT INTO users SET name_user = 'Руслан', email_user = 'warrior07@mail.ru', password_user = '$2y$10$2OxpEH7narYpkOT1H5cApezuzh10tZEEQ2axgFOaKW.55LxIJBgWW', reg_date_user = '12.05.2018', contact_user = '+79117175060';
+INSERT INTO users SET name_user = 'Игнат', email_user = 'ignat.v@gmail.com', password_user = '$2y$10$OqvsKHQwr0Wk6FMZDoHo1uHoXd4UdxJG/5UDtUiie00XaxMHrW8ka', reg_date_user = '2018-05-09 22:11:43', contact_user = '+79212343456';
+INSERT INTO users SET name_user = 'Леночка', email_user = 'kitty_93@li.ru', password_user = '$2y$10$bWtSjUhwgggtxrnJ7rxmIe63ABubHQs0AS0hgnOo41IEdMHkYoSVa', reg_date_user = '2018-05-11 12:15:21', contact_user = '+78202254822';
+INSERT INTO users SET name_user = 'Руслан', email_user = 'warrior07@mail.ru', password_user = '$2y$10$2OxpEH7narYpkOT1H5cApezuzh10tZEEQ2axgFOaKW.55LxIJBgWW', reg_date_user = '2018-05-12 16:03:10', contact_user = '+79117175060';
 
 -- Добавление проектов
 INSERT INTO projects SET name_project = 'Входящие', user_id = 2 ;
@@ -11,36 +11,28 @@ INSERT INTO projects SET name_project = 'Домашние дела', user_id = 3
 INSERT INTO projects SET name_project = 'Авто', user_id = 3;
 
 -- Добавление задач пользователей
-INSERT INTO tasks SET user_id = 1 , project_id = 2, name_task = 'Собеседование в IT компании', create_date_task = '17.05.2018', run_date_task = '', limit_date_task = '20.05.2018', file_task = '', status = FALSE;
-INSERT INTO tasks SET user_id = 1 , project_id = 2, name_task = 'Выполнить тестовое задание', create_date_task = '14.05.2018', run_date_task = '', limit_date_task = '18.05.2018', file_task = '', status = FALSE;
-INSERT INTO tasks SET user_id = 1 , project_id = 2, name_task = 'Сделать задание первого раздела', create_date_task = '15.05.2018', run_date_task = '', limit_date_task = '19.05.2018', file_task = '', status = FALSE;
-INSERT INTO tasks SET user_id = 2 , project_id = 3, name_task = 'Встреча с другом', create_date_task = '13.05.2018', run_date_task = '', limit_date_task = '18.05.2018', file_task = '', status = FALSE;
-INSERT INTO tasks SET user_id = 2 , project_id = 1, name_task = 'Купить корм для кота', create_date_task = '14.05.2018', run_date_task = '', limit_date_task = '21.05.2018', file_task = '', status = FALSE;
-INSERT INTO tasks SET user_id = 3 , project_id = 4, name_task = 'Заказать пиццу', create_date_task = '16.05.2018', run_date_task = '', limit_date_task = '19.05.2018', file_task = '', status = FALSE;
+INSERT INTO tasks SET user_id = 1 , project_id = 2, name_task = 'Собеседование в IT компании', create_date_task = '2018-05-17 18:20:13', run_date_task = NULL, limit_date_task = '2018-05-20 21:13:57', file_task = '', status = FALSE;
+INSERT INTO tasks SET user_id = 1 , project_id = 2, name_task = 'Выполнить тестовое задание', create_date_task = '2018-05-14 18:11:11', run_date_task = NULL, limit_date_task = '2018-05-18 22:23:21', file_task = '', status = FALSE;
+INSERT INTO tasks SET user_id = 1 , project_id = 2, name_task = 'Сделать задание первого раздела', create_date_task = '2018-05-15 03:20:17', run_date_task = NULL, limit_date_task = '2018-05-19 05:22:17', file_task = '', status = FALSE;
+INSERT INTO tasks SET user_id = 2 , project_id = 3, name_task = 'Встреча с другом', create_date_task = '2018-05-13 07:23:28', run_date_task = NULL, limit_date_task = '2018-05-18 10:13:39', file_task = '', status = FALSE;
+INSERT INTO tasks SET user_id = 2 , project_id = 1, name_task = 'Купить корм для кота', create_date_task = '2018-05-14 7:39:29', run_date_task = NULL, limit_date_task = '2018-5-21 23:22:19', file_task = '', status = FALSE;
+INSERT INTO tasks SET user_id = 3 , project_id = 4, name_task = 'Заказать пиццу', create_date_task = '2018-5-16 01:23:48', run_date_task = NULL, limit_date_task = '2018-05-19 21:17:42', file_task = '', status = FALSE;
 
 /*
  Запросы на отображение данных
 */
 
 -- получить список из всех проектов для одного пользователя;
-SELECT name_project FROM projects WHERE user_id IN
-  (SELECT id FROM users WHERE name_user = 'Игнат');
+SELECT name_project FROM projects WHERE user_id = 1;
 
 -- получить список из всех задач для одного проекта;
-SELECT name_task FROM tasks WHERE project_id IN
-  (SELECT id FROM projects WHERE name_project = 'Работа');
+SELECT name_task FROM tasks WHERE project_id = 2;
 
 -- пометить задачу как выполненную;
-/*
-  Еще нужно дополнительно добавить дату выполнения задачи run_date_task, это реализовывать уже посредством PHP?
- */
 UPDATE tasks SET status = TRUE WHERE id = 3;
 
 -- получить все задачи для завтрашнего дня;
-/*
-  Как определить завтрашний день в MySQL. Или тоже реализовывать посредством PHP?
- */
-SELECT name_task FROM tasks WHERE limit_date_task = '19.05.2018';
+SELECT name_task FROM tasks WHERE limit_date_task BETWEEN '2018-05-19 23:59:59' AND '2018-05-21 23:59:59';
 
 -- обновить название задачи по её идентификатору.
 UPDATE tasks SET name_task = 'Новая задача'

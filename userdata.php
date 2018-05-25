ddea2ac3498e90e60ef6c1fcf2cc780db2b6f888
SergWulf <36727388+SergWulf@users.noreply.github.com>
2018-05-25 20:51:30 +0300

    Переименовал data в userdata

--- userdata.php
+++ userdata.php
@@ -1,20 +1,29 @@
 <?php
-
-// пользователи для аутентификации
-$users = [
-    [
-        'email' => 'ignat.v@gmail.com',
-        'name' => 'Игнат',
-        'password' => '$2y$10$OqvsKHQwr0Wk6FMZDoHo1uHoXd4UdxJG/5UDtUiie00XaxMHrW8ka'
-    ],
-    [
-        'email' => 'kitty_93@li.ru',
-        'name' => 'Леночка',
-        'password' => '$2y$10$bWtSjUhwgggtxrnJ7rxmIe63ABubHQs0AS0hgnOo41IEdMHkYoSVa'
-    ],
-    [
-        'email' => 'warrior07@mail.ru',
-        'name' => 'Руслан',
-        'password' => '$2y$10$2OxpEH7narYpkOT1H5cApezuzh10tZEEQ2axgFOaKW.55LxIJBgWW'
-    ]
+/**
+ * В этом файле хранятся необходимые данные для пользователя
+ */
+$name_user = '';
+$project_array = array();
+$array_tasks = array();
+define('PROJECT_ALL', -1);
+$user_id = 0;
+$show_complete_tasks = 1;
+$current_project_id = PROJECT_ALL;
+$errors_form_task = [];
+$data_fields_form_task = [];
+$errors_form_register = [];
+$data_fields_form_register = [];
+$errors_form_auth = [];
+$data_user_form_auth = [];
+$data_user = [];
+$count_projects_array = 0;
+$errors_form_project = [];
+$data_fields_form_project = [];
+$filter_task = 0;
+$filter_tasks = [
+    'today' => 1,
+    'tomorrow' => 2,
+    'overdue' => 3
 ];
+
+?>
